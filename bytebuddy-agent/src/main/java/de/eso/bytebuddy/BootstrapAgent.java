ed30b4437b03e946edb6ba0eb062a177afa891e1
Sergej Isbrecht <sergej.isbrecht@gmail.com>
2018-12-27 00:50:12 +0100

    Add Proxy#newProxyInstance interceptor

--- bytebuddy-agent/src/main/java/de/eso/bytebuddy/BootstrapAgent.java
+++ bytebuddy-agent/src/main/java/de/eso/bytebuddy/BootstrapAgent.java
@@ -5,27 +5,52 @@ import net.bytebuddy.asm.Advice;
 
 import java.io.File;
 import java.lang.instrument.Instrumentation;
+import java.lang.reflect.Proxy;
 import java.nio.file.Files;
+import java.util.Arrays;
 
 import static net.bytebuddy.matcher.ElementMatchers.*;
 
 public final class BootstrapAgent {
 
-  public static void premain(String arg, Instrumentation inst) throws Exception {
+  public static void premain(String arg, Instrumentation instrumentation) throws Exception {
     File tempFolder = Files.createTempDirectory("tmp").toFile();
 
-    new AgentBuilder.Default() //
-        .disableClassFormatChanges() //
-        .ignore(none())
-        .with(AgentBuilder.InitializationStrategy.NoOp.INSTANCE) //
-        .with(AgentBuilder.RedefinitionStrategy.REDEFINITION) //
-        .with(AgentBuilder.TypeStrategy.Default.REDEFINE) //
-        .enableBootstrapInjection(inst, tempFolder) //
-        .type(is(Class.class)) //
+    AgentBuilder agentBuilder =
+        new AgentBuilder.Default() //
+            .disableClassFormatChanges() //
+            .ignore(none())
+            .with(AgentBuilder.InitializationStrategy.NoOp.INSTANCE)
+            .with(AgentBuilder.RedefinitionStrategy.REDEFINITION)
+            .with(AgentBuilder.TypeStrategy.Default.REDEFINE)
+            .enableBootstrapInjection(instrumentation, tempFolder);
+
+    agentBuilder
+        .type(is(Class.class))
         .transform(
             (builder, typeDescription, classLoader, module) ->
                 builder.visit(Advice.to(ClassForNameInterceptor.class).on(named("forName"))))
-        .installOn(inst);
+        .installOn(instrumentation);
+
+    agentBuilder
+        .type(is(Proxy.class))
+        .transform(
+            (builder, typeDescription, classLoader, module) ->
+                builder.visit(
+                    Advice.to(ProxyNewProxyInstanceInterceptor.class)
+                        .on(named("newProxyInstance"))))
+        .installOn(instrumentation);
+  }
+
+  /**
+   * public static Object newProxyInstance(ClassLoader loader, Class<?>[] interfaces,
+   * InvocationHandler h)
+   */
+  static class ProxyNewProxyInstanceInterceptor {
+    @Advice.OnMethodEnter
+    static void intercept(@Advice.Argument(1) Class<?>[] interfaces) {
+      System.out.println("Proxy#newProxyInstance " + Arrays.toString(interfaces));
+    }
   }
 
   static class ClassForNameInterceptor {
@@ -35,7 +60,7 @@ public final class BootstrapAgent {
       // currently I do not know how to use outside variables. Any use from outside the static
       // method will result in an Error
 
-      System.out.println("INTERCEPTEDDDDDDDD " + s);
+      System.out.println("Class#forName " + s);
     }
   }
 }
